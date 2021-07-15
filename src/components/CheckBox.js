c9758b22347e20a119c022671875f7742ecbb1de
may <wijitra.r@softsq.com>
2021-07-16 00:50:48 +0700

    add view box

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
-export default function CheckBox(props) {
+const CheckBox = props => {
   return (
     <TouchableOpacity onPress={props.onChange}>
-      <View>
-        {props.value && <Icon size={24} color="black" name="ticket-alt" />}
+      <View style={styles.iconBox}>
+        {props.value && (
+          <Icon style={styles.icon} size={20} color="white" name="check" />
+        )}
       </View>
     </TouchableOpacity>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  iconBox: {
+    borderRadius: 50,
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    width: 22,
+    height: 22,
+    marginRight: 10,
+  },
+  icon: {
+    backgroundColor: '#9acd32',
+    borderRadius: 50,
+    width: 20,
+    height: 20,
+  },
+});
+
+export default CheckBox;
