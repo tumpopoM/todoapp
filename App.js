962f0e504be57e5ecc44505521ec7f2c70f59173
may <wijitra.r@softsq.com>
2021-07-17 14:07:47 +0700

    add scrollview, add box addtask, set function check box, set function delete

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-import React, {useCallback} from 'react';
-import {View, Text, StyleSheet, Alert, TouchableOpacity} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Input from './src/components/Input';
 import Items from './src/components/Items';
 
-const todos = [
+const initTodos = [
   {
     id: 1,
     title: 'Play Game',
@@ -37,13 +45,31 @@ const todos = [
 ];
 
 const App = () => {
+  const [isShowAddTasks, setIsShowAddTasks] = useState(false);
+  const [todos, setTodos] = useState(initTodos);
+
+  const onPressShowAddTasks = useCallback(
+    item => {
+      return setIsShowAddTasks(!isShowAddTasks);
+    },
+    [isShowAddTasks],
+  );
+
   const onPressAddTasks = useCallback(item => {
     return Alert.alert('onPressAddTasks');
   }, []);
 
-  const onPressCheckBox = useCallback(item => {
-    return Alert.alert('onPressCheckBox');
-  }, []);
+  const onPressCheckBox = useCallback(
+    item => {
+      const data = [...todos];
+      const index = data.findIndex(i => {
+        return i.id === item.id;
+      });
+      data[index].isDone = !data[index].isDone;
+      setTodos(data);
+    },
+    [todos],
+  );
 
   const onPressItem = useCallback(item => {
     return Alert.alert('onPressItem');
@@ -54,27 +80,38 @@ const App = () => {
   }, []);
 
   const onPressDelete = useCallback(item => {
-    return Alert.alert('onPressDelete');
-  }, []);
+    const data = [...todos];
+    const index = data.find(i => {
+      return i.id === item.id;
+    });
+    data.splice(index, 1);
+    setTodos(data);
+    [todos];
+  });
 
   return (
     <View style={styles.container}>
       <View style={styles.titleArea}>
         <Text style={styles.titleText}> Remainder </Text>
-        <TouchableOpacity onPress={onPressAddTasks}>
+        <TouchableOpacity onPress={onPressShowAddTasks}>
           <View style={styles.btnAddTasks}>
             <Icon size={20} color="black" name="plus" />
           </View>
         </TouchableOpacity>
       </View>
       <View style={styles.itemWrapper}>
-        <Items
-          data={todos}
-          onPressCheckBox={onPressCheckBox}
-          onPressItem={onPressItem}
-          onPressEdit={onPressEdit}
-          onPressDelete={onPressDelete}
-        />
+        <ScrollView>
+          <Items
+            data={todos}
+            onPressCheckBox={onPressCheckBox}
+            onPressItem={onPressItem}
+            onPressEdit={onPressEdit}
+            onPressDelete={onPressDelete}
+          />
+        </ScrollView>
+      </View>
+      <View style={styles.inputContainer}>
+        {isShowAddTasks && <Input onPressAddTasks={onPressAddTasks} />}
       </View>
     </View>
   );
@@ -85,11 +122,11 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     backgroundColor: '#e5e7ea',
+    position: 'relative',
   },
   titleArea: {
     flexDirection: 'row',
     alignItems: 'center',
-    // justifyContent: 'space-between',
     paddingHorizontal: 15,
     paddingVertical: 10,
     marginTop: 10,
@@ -113,6 +150,16 @@ const styles = StyleSheet.create({
     shadowColor: '#000000',
     shadowOffset: {width: 0, height: 2},
     elevation: 5,
+  },
+  inputContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
 
