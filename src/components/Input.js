713d37cb4181609602ad9512294ac7e15456529e
may <wijitra.r@softsq.com>
2021-07-19 00:17:01 +0700

    add keybordavoidingview

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,63 +6,73 @@ import {
   TouchableOpacity,
   Text,
   Alert,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 
 const Input = props => {
   return (
-    <View style={styles.inputWrapper}>
-      <View style={styles.inputTitle}>
-        <TextInput
-          placeholder="Title"
-          value={props.valueTitle}
-          maxLength={40}
-          onChangeText={title => {
-            props.setInputTitle(title);
-          }}
-        />
-      </View>
-      <View style={styles.inputDescription}>
-        <TextInput
-          placeholder="Description"
-          multiline={true}
-          numberOfLines={5}
-          textAlign={'left'}
-          textAlignVertical={'top'}
-          value={props.valueDescription}
-          onChangeText={description => {
-            props.setInputDescription(description);
-          }}
-        />
-      </View>
-      {props.statusAction === 'add' && (
-        <TouchableOpacity
-          onPress={() => {
-            if (props.valueTitle.length > 0) {
-              props.onPressAddTasks();
-            } else {
-              Alert.alert('Please Enter Title');
-            }
-          }}>
-          <View style={styles.buttonAdd}>
-            <Text style={styles.buttonText}>Add</Text>
+    <KeyboardAvoidingView behavior={'padding'} style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inputWrapper}>
+          <View style={styles.inputTitle}>
+            <TextInput
+              placeholder="Title"
+              value={props.valueTitle}
+              maxLength={40}
+              onChangeText={title => {
+                props.setInputTitle(title);
+              }}
+            />
           </View>
-        </TouchableOpacity>
-      )}
-      {props.statusAction === 'edit' && (
-        <TouchableOpacity
-          onPress={() => {
-            props.onPressEdit(props.idEdit);
-          }}>
-          <View style={styles.buttonAdd}>
-            <Text style={styles.buttonText}>Save</Text>
+          <View style={styles.inputDescription}>
+            <TextInput
+              placeholder="Description"
+              multiline={true}
+              numberOfLines={5}
+              textAlign={'left'}
+              textAlignVertical={'top'}
+              value={props.valueDescription}
+              onChangeText={description => {
+                props.setInputDescription(description);
+              }}
+            />
           </View>
-        </TouchableOpacity>
-      )}
-    </View>
+          {props.statusAction === 'add' && (
+            <TouchableOpacity
+              onPress={() => {
+                if (props.valueTitle.length > 0) {
+                  props.onPressAddTasks();
+                } else {
+                  Alert.alert('Please Enter Title');
+                }
+              }}>
+              <View style={styles.buttonAdd}>
+                <Text style={styles.buttonText}>Add</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          {props.statusAction === 'edit' && (
+            <TouchableOpacity
+              onPress={() => {
+                props.onPressEdit(props.idEdit);
+              }}>
+              <View style={styles.buttonAdd}>
+                <Text style={styles.buttonText}>Save</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%'
+  },
   inputWrapper: {
     paddingVertical: 10,
     paddingHorizontal: 5,
